e29a52d1b547abde2cf361f4a7c95b62ae9b132b
Saragorule13 <gorulesaravk13@gmail.com>
2025-02-04 22:16:44 +0530

    Worked on landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className='font-semibold'>
-      Hello
+    <div className=''>
+      <Home/>
     </div>
   )
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/HomeComponents/Navbar.home'
+import Hero from '../components/HomeComponents/Hero.home'
+
+function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Home
